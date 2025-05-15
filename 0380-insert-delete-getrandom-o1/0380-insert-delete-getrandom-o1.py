f524cb7358a1515670afb98cc308bc7866987fef
Ahib Afnan Siam <73303361+Ahib-Afnan-Siam@users.noreply.github.com>
2025-05-15 21:01:45 +0600

    Time: 137 ms (52.21%), Space: 57.7 MB (91.86%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -26,3 +26,8 @@ class RandomizedSet:
 
     def getRandom(self) -> int:
         return random.choice(self.values)
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
