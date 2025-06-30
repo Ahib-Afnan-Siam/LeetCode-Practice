54abd27a72675f7ca2d09d4d710c40e2104cf1d7
Ahib Afnan Siam <73303361+Ahib-Afnan-Siam@users.noreply.github.com>
2025-06-30 17:23:47 +0600

    Time: 88 ms (84.35%), Space: 18.6 MB (66.29%) - LeetHub

--- 0772-construct-quad-tree/0772-construct-quad-tree.py
+++ 0772-construct-quad-tree/0772-construct-quad-tree.py
@@ -13,32 +13,26 @@ class Node:
 class Solution:
     def construct(self, grid: List[List[int]]) -> 'Node':
         n = len(grid)
-
-        def build_quad_tree(r_start, c_start, size):
-            # Check if all values in the current sub-grid are the same
-            first_val = grid[r_start][c_start]
-            all_same = True
-            for r in range(r_start, r_start + size):
-                for c in range(c_start, c_start + size):
-                    if grid[r][c] != first_val:
-                        all_same = False
+        
+        def build(r, c, size):
+            total = grid[r][c]
+            same = True
+            for i in range(r, r + size):
+                for j in range(c, c + size):
+                    if grid[i][j] != total:
+                        same = False
                         break
-                if not all_same:
+                if not same:
                     break
-
-            # If all values are the same, create a leaf node
-            if all_same:
-                return Node(val=bool(first_val), isLeaf=True, topLeft=None, topRight=None, bottomLeft=None, bottomRight=None)
             
-            # If values are different, create a non-leaf node and recurse for children
-            else:
-                half_size = size // 2
-                top_left = build_quad_tree(r_start, c_start, half_size)
-                top_right = build_quad_tree(r_start, c_start + half_size, half_size)
-                bottom_left = build_quad_tree(r_start + half_size, c_start, half_size)
-                bottom_right = build_quad_tree(r_start + half_size, c_start + half_size, half_size)
-                
-                # The val for a non-leaf node can be anything; we'll just set it to True (or False, either is accepted)
-                return Node(val=True, isLeaf=False, topLeft=top_left, topRight=top_right, bottomLeft=bottom_left, bottomRight=bottom_right)
-
-        return build_quad_tree(0, 0, n)
+            if same:
+                return Node(bool(total), True, None, None, None, None)
+            
+            half = size // 2
+            topLeft = build(r, c, half)
+            topRight = build(r, c + half, half)
+            bottomLeft = build(r + half, c, half)
+            bottomRight = build(r + half, c + half, half)
+            return Node(True, False, topLeft, topRight, bottomLeft, bottomRight)
+        
+        return build(0, 0, n)
