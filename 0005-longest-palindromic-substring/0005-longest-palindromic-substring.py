4be6c288152ff22c98b9f2f890d083e771cec129
Ahib Afnan Siam <73303361+Ahib-Afnan-Siam@users.noreply.github.com>
2025-07-01 18:00:28 +0600

    Time: 263 ms (72.88%), Space: 17.8 MB (60.07%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def expandAroundCenter(left: int, right: int) -> str:
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]  # Valid palindrome after overshooting
+
+        longest = ""
+        for i in range(len(s)):
+            # Odd-length palindrome
+            p1 = expandAroundCenter(i, i)
+            # Even-length palindrome
+            p2 = expandAroundCenter(i, i + 1)
+            # Update longest
+            if len(p1) > len(longest):
+                longest = p1
+            if len(p2) > len(longest):
+                longest = p2
+
+        return longest
