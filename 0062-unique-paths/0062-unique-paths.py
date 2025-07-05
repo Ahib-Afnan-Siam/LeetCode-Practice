43bf1210eaea90c159f848324c0ffc229ea95222
Ahib Afnan Siam <73303361+Ahib-Afnan-Siam@users.noreply.github.com>
2025-07-05 17:41:04 +0600

    Time: 0 ms (100%), Space: 18 MB (25.89%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,8 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        total_moves = m + n - 2
+        k = min(m - 1, n - 1)
+        result = 1
+        for i in range(1, k + 1):
+            result = result * (total_moves - i + 1) // i
+        return result
