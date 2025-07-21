db38d86b03f187493bc1e33ff641f2fe0770514d
Ahib Afnan Siam <73303361+Ahib-Afnan-Siam@users.noreply.github.com>
2025-07-21 09:00:56 +0600

    Time: 288 ms (84.89%), Space: 19.7 MB (52.29%) - LeetHub

---
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        res = []
+        for c in s:
+            if len(res) >= 2 and res[-1] == c and res[-2] == c:
+                continue
+            else:
+                res.append(c)
+        return ''.join(res)
